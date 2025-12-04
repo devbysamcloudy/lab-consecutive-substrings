11a960b7568938b856372fbaa56d42cd6cd86f2d
Samuel Ng'ang'a <166153002+devbysamcloudy@users.noreply.github.com>
2025-12-04 15:28:43 +0300

    updated js

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,18 +1,25 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  const result = [];
+
+  for (let i = 0; i < string.length; i++) {
+    let substring = "";
+    for (let j = i; j < string.length; j++) {
+      substring += string[j];
+      result.push(substring);
+    }
+  }
+
+  return result;
 }
 
 if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
+  console.log("Expected: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
   console.log("=>", consecutiveSubstrings('abc'));
 
   console.log("");
 
-  console.log("Expecting: ['a']");
+  console.log("Expected: ['a']");
   console.log("=>", consecutiveSubstrings('a'));
 }
 
 module.exports = consecutiveSubstrings;
-
-
